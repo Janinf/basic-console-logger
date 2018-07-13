1f480de6caf31b0eac9feee9dac3e9cbd3963100
Jan Armbruster <armbruster.dev@gmail.com>
2018-07-13 12:53:04 +0200

    Removed ansii color codes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # basic-console-logger
-A simple console logger using ansii color codes.
+A simple console logger with timestamp.
 
 ## Installation
 `npm install basic-console-logger`
@@ -12,8 +12,10 @@ const logger = require('basic-console-logger');
 
 logger.info('Some information');
 logger.warning('This is a warning');
-logger.error('This is an error');
+logger.error(new Error('This is an error'));
 ```
+
+![Example result for log levels](https://github.com/janinf/basic-console-logger/images/log-levels.png)
 
 ### Log Requests
 ```
@@ -30,24 +32,26 @@ app.use((req, res, next) => {
 app.listen(3000);
 ```
 
+![Example result for log levels](https://github.com/janinf/basic-console-logger/images/request.png)
+
 ## API
 ### Methods
 `info(message)`
-Logs a green message with a timestamp to the console
+Logs an info message with a timestamp to the console
 * Parameters
     * <code>message</code>: A message of type string
     
 `warning(message)`
-Logs a yellow message with a timestamp to the console
+Logs a warning message with a timestamp to the console
 * Parameters
     * <code>message</code>: A message of type string
     
 `error(error)`
-Logs a red message with a timestamp to the console
+Logs an error message with a timestamp to the console
 * Parameters
     * <code>error</code>: An error or a message of type string
     
 `request(req)`
-Logs an http request to the console in cyan. Format: \<method> \<path>
+Logs an http request to the console. Format: \<method> \<path>
 * Parameters
     * <code>req</code>: An express request object.
--- index.js
+++ index.js
@@ -1,42 +1,4 @@
 /**
- * Ansii escape code to reset all colors and styles.
- * @type {string}
- */
-const ansiiResetColors = '\x1b[0m';
-
-/**
- * Ansii escape codes for foreground colors.
- * @type {{red: string, green: string, yellow: string, cyan: string}}
- */
-const ansiiColorCodes = {
-    red: '\x1b[31m',
-    green: '\x1b[32m',
-    yellow: '\x1b[33m',
-    cyan: '\x1b[36m'
-};
-
-/**
- * Checks if a string is a valid ansii color code.
- * @param code {string} Ansii color code.
- * @returns {boolean} True if code is valid.
- */
-function isValidAnsiiColorCode(code) {
-    return !!Object.values(ansiiColorCodes).find(c => c === code);
-}
-
-/**
- *
- * @param text {string}
- * @param color
- * @returns {string}
- */
-function colorText(text, color) {
-    if (!isValidAnsiiColorCode(color)) throw new Error(`No valid ansii color code, known codes: ${Object.values(ansiiColorCodes).join(', ')}`);
-
-    return `${color}${text}${ansiiResetColors}`;
-}
-
-/**
  * Adds a timestamp in front of a string.
  * @param text {string} Any string.
  * @returns {string} String with timestamp, format: [<timestamp>] <text>
@@ -54,23 +16,23 @@ module.exports = {
      * Add an info level log entry.
      * @param message {string} Message to log.
      */
-    info: message => console.log(colorText(timestampText(message), ansiiColorCodes.green)),
+    info: message => console.info(timestampText(`Info: ${message}`)),
     /**
      * Add a warning level log entry.
      * @param message {string} Message to log.
      */
-    warning: message => console.log(colorText(timestampText(message), ansiiColorCodes.yellow)),
+    warning: message => console.warn(timestampText(`Warning: ${message}`)),
     /**
      * Add an error level log entry.
      * @param error {string|Error} Message or error to log.
      */
     error: error => {
-        const message = error instanceof Error ? error.stack : error;
-        console.log(colorText(timestampText(message), ansiiColorCodes.red));
+        const message = error instanceof Error ? error.stack : `Error: ${error}`;
+        console.error(timestampText(message));
     },
     /**
      * Logs a request.
      * @param req {Request} Express request object.
      */
-    request: req => console.log(colorText(timestampText(`${req.method} ${req.url}`), ansiiColorCodes.cyan))
+    request: req => console.log(timestampText(`${req.method} ${req.url}`))
 };
